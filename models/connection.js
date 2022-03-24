6060673171b00e10314027ffd734e42b8c3525f2
Régis GRARE <regisgrare@hotmail.fr>
2022-03-24 11:38:38 +0100

    heroku fixed

--- models/connection.js
+++ models/connection.js
@@ -12,7 +12,7 @@ mongoose.connect('srv://chmick:Nd15091993*@cluster0.ojslq.mongodb.net/veazit?ret
     options,
     function (err) {
         if (err) {
-            console.log(error, 'failed to connect to the database because');
+            console.log(err, 'failed to connect to the database because');
         } else {
             console.log(' Database Veazit connection : Success ');
         }
