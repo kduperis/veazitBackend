33b38f8d13070360708658ea24275b9ef1542e1e
Nicolas Debray <nikola.debray@hotmail.fr>
2022-03-24 13:33:38 +0100

    test routes

--- routes/index.test.js
+++ routes/index.test.js
@@ -2,7 +2,27 @@ var app = require("../app")
 var request = require("supertest")
 
 
-//NE PAS OUBLIER DE RAJOUTER DANS LE SCRIPT DE APP.JS 'TEST':'JEST'
+test("Récupération des infos des utilisateurs", async () => {
+  await request(app).get('/best-users')
+    .query({ token: '182sw8KccpiKGO3_wHdMWmIiYFQjCYwP' })
+    .expect(200)
+});
+
+test("Récupération des tous les badges en Base De Données", async () => {
+  await request(app).get('/badgesData')
+    .query({ token: '182sw8KccpiKGO3_wHdMWmIiYFQjCYwP' })
+    .expect(200)
+});
+
+test("Tentative connexion sans renseigner d'email", async () => {
+  await request(app).post('/users/sign-up')
+    .send({ password: 'nicolas' })
+    .expect(200)
+    .expect({ result: false, error: ['Veuillez indiquer un mail'] })
+});
+
+
+/* //NE PAS OUBLIER DE RAJOUTER DANS LE SCRIPT DE APP.JS 'TEST':'JEST'
 
 test("Validation de la prise en compte d'un lieu visité", async () => {
   await request(app).update('/user-update')
@@ -31,3 +51,4 @@ test("Vérifier que la Map renvoie correctement toutes les informations des POI"
     .expect(200)
     .expect({ result: true, POI });
 });
+ */
